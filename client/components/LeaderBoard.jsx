bc2ddb55a5b78a983765d99f5a87f91b59779626
Chris <Chris@MacBook-Pro-5.fios-router.home>
2020-10-06 19:06:44 -0400

    fixed flip bug

--- client/components/LeaderBoard.jsx
+++ client/components/LeaderBoard.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 
 const LeaderBoard = (props) => {
-  const bestRecordsElem = props.leaderBoard.bestRecords.map((bestRecord, i) => {
-    return (
-      <li key={i}>
-        {bestRecord.username}: {bestRecord.bestRecord}
-      </li>
-    );
-  });
-  const mostPlayedElem = props.leaderBoard.mostPlayed.map((mostplay, i) => {
-    return (
-      <li key={i}>
-        {mostplay.username}: {mostplay.played}
-      </li>
-    );
-  });
+  // const bestRecordsElem = props.leaderBoard.bestRecords.map((bestRecord, i) => {
+  //   return (
+  //     <li key={i}>
+  //       {bestRecord.username}: {bestRecord.bestRecord}
+  //     </li>
+  //   );
+  // });
+
+  // const mostPlayedElem = props.leaderBoard.mostPlayed.map((mostplay, i) => {
+  //   return (
+  //     <li key={i}>
+  //       {mostplay.username}: {mostplay.played}
+  //     </li>
+  //   );
+  // });
+
   return (
     <div className="recordsContainer">
       <h3>LeaderBoard</h3>
       <div className="records">
         <ol className="records-best-record">
           <label>Best Record</label>
-          {bestRecordsElem}
+          {/* {bestRecordsElem} */}
         </ol>
         <ol className="records-most-played">
           <label>Most Played</label>
-          {mostPlayedElem}
+          {/* {mostPlayedElem} */}
         </ol>
       </div>
     </div>
